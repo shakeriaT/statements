71a33695571b06d1514e2f17a9b657787d0203fc
shakeriaT <155984698+shakeriaT@users.noreply.github.com>
2024-03-11 16:00:15 +0000

    number charges of wood and color

---
+++ main.py
@@ -0,0 +1,15 @@
+# assigning variables to given values
+numChar = 20
+charge = 0.00
+color = 'gold'
+woodType = 'pine'
+# check if number of characters is greater than 5, add $4 for every extra character
+if numChar > 5: charge += (numChar - 5) * 4
+# if wood is of oak add charge by 20
+if woodType == 'oak': charge += 20
+# if coloring is gold add charge by 15
+if color == 'gold': charge += 15
+# add charge by 35 as minimum charge  
+charge += 35
+#print the charge to screen
+print("The charge for this sign is $%.1f", charge)
